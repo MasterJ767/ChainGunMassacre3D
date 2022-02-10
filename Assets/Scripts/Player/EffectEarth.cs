8081b4d6bce499cd2432e6a7fec699242f7b8b48
MasterJ767 <jon.arundel@hotmail.com>
2022-02-10 13:34:47 +0000

    Created base files for gun effects

---
+++ Assets/Scripts/Player/EffectEarth.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class EffectEarth : MonoBehaviour
+    {
+        public GameObject rockPrefab;
+        
+        public void StartEarthAttack(Collider enemy, EarthParameters parameters, Weapon weapon)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Player/EffectElectric.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class EffectElectric : MonoBehaviour
+    {
+        private LineRenderer[] lrs;
+
+        public void StartLightningAttack(Collider enemy, ElectricParameters parameters, Weapon weapon)
+        {
+            
+        }
+    }
+}
